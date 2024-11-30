25a02f60e27cd2c1f27a1cbfee21b68e599b5691
FrenchelT <ftabilas@hawkmail.hccfl.edu>
2024-11-30 21:14:29 +0000

    20240922TabilasFrenchelCOP1000-Assignment-3-3

--- main.py
+++ main.py
@@ -7,28 +7,42 @@ MIN_YEAR = 0
 MIN_MONTH = 1
 MAX_MONTH = 12
 MIN_DAY = 1
-MAX_DAY = 31
 
-year = None
-month = None
-day = None
+def is_leap_year(year):
+    """Checks if a year is a leap year."""
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
-# Get the year, then the month, then the day
-# housekeeping()
+def get_max_days(month, year):
+    """Returns the maximum number of days in a month for a given year."""
+    if month == 2: 
+        if is_leap_year(year):
+            return 29
+        else:
+            return 28
+    elif month in [1, 3, 5, 7, 8, 10, 12]: 
+        return 31
+    else:
+        return 30
 
+# Get User Input
 month = int(input("Enter a month: "))
 day = int(input("Enter a day: "))
 year = int(input("Enter a year: "))
 
 # Check to be sure date is valid
 
-if int(year) <= MIN_YEAR: # invalid year
+if year <= MIN_YEAR:  # Invalid year
     validDate = False
-elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
+elif month < MIN_MONTH or month > MAX_MONTH:  # Invalid month
     validDate = False
-elif int(day) < MIN_DAY or int(day) > MAX_DAY: # invalid day
-    validDate = False
+else:
+    max_days = get_max_days(month, year)
+    if day < MIN_DAY or day > max_days:  # Invalid day
+        validDate = False
+    else:
+        validDate = True
 
+        
 # Test to see if date is valid and output date and whether it is valid or not
 
 # endOfJob()
